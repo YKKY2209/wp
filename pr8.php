66e744f21e2fd7ab7350787bd620530f4f02459c
YKKY2209 <129202079+YKKY2209@users.noreply.github.com>
2023-10-11 05:54:47 +0530

    Add files via upload

---
+++ pr8.php
@@ -0,0 +1,20 @@
+<?php
+echo strlen("Hello World!");
+echo str_replace("world","SYCS","Hello");
+ehco str_word_count("Hello World!");
+echo str rev("Hello World");
+echo strpos("Hello World!","World");
+?>
+
+
+
+
+<?php
+echo abs(-9.55);
+echo floor(9.5);
+echo coil(9.5);
+echo max(10,4,2);
+echo min(/10,4,2);
+echo pow(4,2);
+echo sqrt (25);
+?>
